91636fe2f8e4afacb0ea7d96cc09c78e68226fca
Kamal-Baraili <kamalbaraili86@gmail.com>
2024-09-11 16:36:43 +0545

    JS variables demo

---
+++ JS variables/script.js
@@ -0,0 +1,30 @@
+//Variables
+
+//var -> Global
+
+var age = 22;
+var num1 = 10;
+var num2 = 11;
+
+console.log(age)
+
+console.log(num1 + num2)
+
+//Naming convention for variables -> use camel case
+
+var firstName = "Kamal"
+console.log(firstName)
+
+
+//let -> Local
+
+if (true) {
+    let a = 5;
+    console.log(a);
+}
+
+//const -> local Note: once value is assigned to const, the value cannot be reassigned
+
+const x = 25;
+
+console.log(x)
