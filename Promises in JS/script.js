2c64d1341844c6d7a79c8eca6ec881862443c0e5
Kamal-Baraili <kamalbaraili86@gmail.com>
2024-09-13 14:30:35 +0545

    Promises in JS

---
+++ Promises in JS/script.js
@@ -0,0 +1,15 @@
+//Promises in JS
+let a = 10;
+let b = 20;
+
+let result = a + b;
+
+async function getData() { 
+    let resultFromServer = await fetch (
+        "https://jsonplaceholder.typicode.com/posts"
+    );
+    console.log(await resultFromServer.json());
+}
+
+getData();
+console.log(result);
