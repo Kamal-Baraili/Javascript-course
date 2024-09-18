51fb66acd60b310c48bbcc969f9b9cb56a942ffc
Kamal-Baraili <kamalbaraili86@gmail.com>
2024-09-18 17:00:27 +0545

    JS Arrays

--- JS Arrays/script.js
+++ JS Arrays/script.js
@@ -18,17 +18,22 @@ myArray.push({name: "Kamal"});
 console.log(myArray);
 console.log(myArray.indexOf("Hi"));
 
-//Example 1:
+//Example 1: Accessing elements of an Array
 let fruits = ['apple', 'banana', 'cherry'];
 console.log(fruits[0]); // Output: 'apple'
 console.log(fruits[1]);
 
-//Example 2:
+//Example 2: Popping element from an array
 let vehicles = ['car', 'bike', 'truck'];
 vehicles.pop();
 console.log(vehicles);
 
-//Example 3:
+//Example 3: Modifying the elements of an array
+let colors = ['red', 'green', 'blue'];
+colors[1] = 'yellow';
+console.log(colors);
+
+//Example 4: Looping an array
 let names = ['Alice', 'Bob', 'Charlie'];
 for (let i = 0; i < names.length; i++) {
     console.log("Hi" + names[i]);
