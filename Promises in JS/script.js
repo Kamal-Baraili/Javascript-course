06eef82e4f801b8602d42436f8d803a10047c209
Kamal-Baraili <kamalbaraili86@gmail.com>
2024-09-22 20:26:21 +0545

    Promises in JS

--- Promises in JS/script.js
+++ Promises in JS/script.js
@@ -1,4 +1,26 @@
 //Promises in JS
+// Create a promise
+const myPromise = new Promise((resolve, reject) => {
+    const success = false;
+
+    if (success) {
+        resolve("Promise resolved successfully!");
+    } else {
+        reject("Promise rejected!");
+    }
+});
+
+// Handle the promise
+myPromise.then(result => {
+        console.log(result); // Handle the resolved value
+    })
+    
+myPromise.catch(error => {
+        console.error(error); // Handle the error or rejected value
+    });
+
+
+//Using Async and await
 let a = 10;
 let b = 20;
 
