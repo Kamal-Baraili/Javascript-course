de223b85a1f7dcf0b43052ff4fff04307c844b9e
Kamal-Baraili <kamalbaraili86@gmail.com>
2024-09-23 14:12:55 +0545

    Added condition and condition icon

--- Weather-app/script.js
+++ Weather-app/script.js
@@ -4,6 +4,8 @@ const search_button = document.getElementById('city-search');
 const city_location = document.getElementById('city-details');
 const local_time = document.getElementById('local-time');
 const temperature = document.getElementById('temperature');
+const condition = document.getElementById('condition');
+const icon = document.getElementById('icon');
 
 async function getData(city_location) {
     let data = await fetch(
@@ -13,9 +15,11 @@ async function getData(city_location) {
 }
 
 search_button.addEventListener("click", async () => {
-    const value = input.value;
-    const result = await getData(value);
+    const val = input.value;
+    const result = await getData(val);
     city_location.innerText = `${result.location.name}, ${result.location.country}`;
-    local_time.innerText = `${result.location.localtime}`;
+    local_time.innerText = `${result.location.localtime} (24-hr)`;
     temperature.innerText = `${result.current.temp_c} \u00B0 C`;
+    condition.innerText = `${result.current.condition.text}`
+    icon.setAttribute("src", `${result.current.condition.icon}`);
 });
