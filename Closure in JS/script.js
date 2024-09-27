336cbe8270e7fa728712b3373554d90a9175afce
Kamal-Baraili <kamalbaraili86@gmail.com>
2024-09-27 19:03:40 +0545

    Closure in JS

--- Closure in JS/script.js
+++ Closure in JS/script.js
@@ -10,3 +10,21 @@ let consoleKamal = main("Kamal");
 
 consoleKamal();
 consoleKamal();
+
+//Example 1: 
+
+function adder(num) {
+    function add(b) {
+        console.log(num + b);
+    }
+    return add;
+}
+
+const addTo5 = adder(5);
+const addTo10 = adder(10);
+
+addTo5(2);
+addTo5(10);
+
+addTo10(2);
+addTo10(10);
